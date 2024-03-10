9897a5bff8970fc340d6dee7ea283c959c1824ee
Hiroaki Suzuki <rirazou05410@gmail.com>
2024-03-10 16:53:29 +0900

    フロント：ユーザー情報の取得画面を作成
    固定のユーザー情報を表示し、API作成後にそちらから取得するように変更する

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,110 @@
+import { useState } from 'react';
 import './App.css';
-import { Button, Center, Container, Heading } from '@chakra-ui/react';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Center,
+  Container,
+  Heading,
+  SimpleGrid,
+  Skeleton,
+  Stack,
+  StackDivider,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setLoading(true);
+    setUsers([]);
+
+    setTimeout(() => {
+      const users: User[] = [
+        {
+          id: 1,
+          name: 'Taro',
+          username: 'taro',
+          email: 'taro@example.com',
+        },
+        {
+          id: 2,
+          name: 'Jiro',
+          username: 'jiro',
+          email: 'jiro@example.com',
+        },
+        {
+          id: 3,
+          name: 'Saburo',
+          username: 'saburo',
+          email: 'saburo@example.com',
+        },
+      ];
+      setUsers(users);
+      setLoading(false);
+    }, 1000);
+  };
+
   return (
     <Container mt={100}>
       <Heading size="xl" mb={4}>
-        タイトル
+        ユーザー情報の取得サンプル
       </Heading>
       <Center>
-        <Button colorScheme="teal">ボタン</Button>
+        <Button colorScheme="teal" onClick={handleClick}>
+          取得
+        </Button>
       </Center>
+
+      <Card mt="20px">
+        <CardHeader>
+          <Heading size="md" textAlign={'center'}>
+            ユーザー一覧
+          </Heading>
+        </CardHeader>
+        <CardBody>
+          {users.length === 0 && !loading ? (
+            <Center>
+              <Text>取得ボタンをクリックしてください</Text>
+            </Center>
+          ) : loading ? (
+            <Stack divider={<StackDivider />}>
+              {[...Array(10)].map((_, index) => (
+                <Skeleton key={index} height="120px" />
+              ))}
+            </Stack>
+          ) : (
+            <VStack align="start" divider={<StackDivider />}>
+              {users.map((user) => (
+                <Stack key={user.id}>
+                  <SimpleGrid columns={2} spacing={2} width="400px">
+                    <Text>ID</Text>
+                    <Text>{user.id}</Text>
+                    <Text>Name</Text>
+                    <Text>{user.name}</Text>
+                    <Text>UserName</Text>
+                    <Text>{user.username}</Text>
+                    <Text>EMail</Text>
+                    <Text>{user.email}</Text>
+                  </SimpleGrid>
+                </Stack>
+              ))}
+            </VStack>
+          )}
+        </CardBody>
+      </Card>
     </Container>
   );
 }
